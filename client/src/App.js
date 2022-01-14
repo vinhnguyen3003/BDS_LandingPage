327e1fcfa2013626c928bb63094e29fe9870b58a
vinhnguyen3003 <khanhvinh30032000@gmail.com>
2022-01-14 10:40:26 +0700

    Update All

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
@@ -7,16 +6,6 @@ import './stylesheets/app.scss';
 import RegisterButton from './components/register-button/register-button';
 
 function App() {
-  
-  // useEffect(()=>{
-  //   let currentPos = 0;
-  //   document.documentElement.addEventListener('wheel', ()=>{
-      
-  //     const wrapperEl = document.getElementsByClassName('section-wrapper')[0];
-  //     currentPos += wrapperEl.scrollHeight;
-  //     document.documentElement.scrollTo(0, currentPos)
-  //   })
-  // },[])
   return (
     <div className="App">
       <BrowserRouter>
